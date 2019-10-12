b12ff2d0e74d7b9e9d8263217bd9cb9f6dcbbbfe
aniruddha <ani@dctacademy.com>
2019-10-12 11:22:00 +0530

    login screen

--- client/src/components/users/Login.js
+++ client/src/components/users/Login.js
@@ -1,10 +1,46 @@
 import React from 'react'
 
 class Login extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        const formData = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(formData)
+    }
+
     render() {
         return (
             <div>
-                <h2>Login with us</h2>
+                <h2>Login</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Email
+                        <input type="text" value={this.state.email} onChange={this.handleChange} name="email" />
+                    </label> <br />
+
+                    <label>
+                        Password
+                        <input type="password" value={this.state.password} onChange={this.handleChange} name="password" />
+                    </label> <br />
+
+                    <input type="submit" />
+                </form>
             </div>
         )
     }
