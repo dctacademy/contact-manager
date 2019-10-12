0524c2d1d533844bf38ce41a2e0be6c293ce8813
aniruddha <ani@dctacademy.com>
2019-10-12 13:59:00 +0530

    links change based on auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
+import isEmpty from 'lodash/isEmpty'
+import { connect } from 'react-redux'
 
 import Home from './components/Home'
 import Register from './components/users/Register'
 import Login from './components/users/Login'
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <div>
           <Link to="/">Home</Link>
-          <Link to="/users/register">Register</Link>
-          <Link to="/users/login">Login</Link>
-
+          {
+            isEmpty(props.user) ? (
+              <div>
+                <Link to="/users/register">Register</Link>
+                <Link to="/users/login">Login</Link>
+              </div> 
+             ) : ( 
+               <div>
+                  <Link to="/users/account">Account</Link>
+                  <Link to="/users/logout">Logout</Link>
+               </div>
+             ) 
+          }
 
           <Route path="/" component={Home} exact={true} />
           <Route path="/users/register" component={Register} />
@@ -22,4 +34,9 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    user: state.user 
+  }
+}
+export default connect(mapStateToProps)(App);
